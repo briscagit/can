dc787fe4fef8eab2c9a0500ff42982954a44c333
briscagit <briscagit@gmail.com>
2018-04-17 20:48:33 -0400

    change canvas width and height

--- scripts/can.js
+++ scripts/can.js
@@ -5,6 +5,11 @@ function randomInt(max) {
 document.onclick = function() {
     var inlineCanvas = document.getElementById("inlineCanvas");
     var canvas = document.createElement("canvas");
+    // 1920 = 2^7 * 3   * 5
+    // 1080 = 2^3 * 3^3 * 5
+    // 2^7 = 128
+    canvas.width = 128
+    canvas.height = 135
     document.body.insertBefore(canvas,inlineCanvas);
     var ctx = canvas.getContext('2d');
     // ctx.fillStyle = 'red';
@@ -12,5 +17,5 @@ document.onclick = function() {
     color = "rgb(" + randomInt(256) + "," + randomInt(256) + "," + randomInt(256) + ")"
     ctx.fillStyle = color
     // fillRect(x,y,width,height)
-    ctx.fillRect(10,10,100,100);
+    ctx.fillRect(0,0,128,135);
 }
