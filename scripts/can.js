d6c79818af7dd4265bb67b5e3e3ead34e67b2e03
briscagit <briscagit@gmail.com>
2018-04-17 20:15:06 -0400

    change color from red to random

--- scripts/can.js
+++ scripts/can.js
@@ -1,14 +1,18 @@
+/*
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
+*/
 
 document.onclick = function() {
     var inlineCanvas = document.getElementById("inlineCanvas");
     var canvas = document.createElement("canvas");
     document.body.insertBefore(canvas,inlineCanvas);
     var ctx = canvas.getContext('2d');
-
-    ctx.fillStyle = 'red';
+    // ctx.fillStyle = 'red';
+    // there is no printf equivalent in js
+    color = "rgb(" + Math.random() + "," + Math.random() + "," + Math.random() + ")"
+    ctx.fillStyle = color
     // fillRect(x,y,width,height)
     ctx.fillRect(10,10,100,100);
 }
